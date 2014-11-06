aa2d78794e5f60f2664f4b5e46decf8d40ac7e19
liuxingghost <10759444413@qq.com>
2014-11-06 20:16:31 +0800

    add && modify files

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "<<< source build/envsetup.sh"
+cd ../ && . build/envsetup.sh && cd -
---
+++ phoneapps.mk
@@ -0,0 +1,27 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps :=		Bluetooth \
+					  		BluetoothExt \
+					  		Camera2 \
+					  		KeyChain \
+					  		CertInstaller \
+					  		Galaxy4 \
+					  		HTMLViewer \
+                      		LatinIME \
+					  		PacProcessor \
+					  		Nfc \
+					  		WAPPushManager \
+                      		Stk
+					  		
+
+private-phone-priv-apps :=  BackupRestoreConfirmation \
+					  		InputDevices \
+					  		DefaultContainerService \
+					  		PicoTts \
+					  		ExternalStorageProvider \
+                      		MediaProvider \
+					  		ProxyHandler \
+                      		SharedStorageBackup \
+					  		Shell \
+					  		Tag
