196ae6a500f15d4fd8841d754dbe73d7a1fde39c
liuxingghost <1075944413@qq.com>
2014-11-07 23:04:04 +0800

    make workspace

---
+++ phoneapps.mk
@@ -0,0 +1,30 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps :=		Bluetooth \
+					  		BluetoothExt \
+					  		Camera2 \
+					  		KeyChain \
+					  		CertInstaller \
+					  		Galaxy4 \
+					  		HTMLViewer \
+                      		LatinIME \
+					  		PacProcessor \
+					  		Nfc \
+					  		WAPPushManager \
+                      		Stk \
+                      		PrintSpooler
+					  		
+
+private-phone-priv-apps :=  BackupRestoreConfirmation \
+					  		InputDevices \
+					  		DefaultContainerService \
+					  		PicoTts \
+					  		ExternalStorageProvider \
+                      		MediaProvider \
+					  		ProxyHandler \
+                      		SharedStorageBackup \
+					  		Shell \
+					  		Tag \
+					  		ThemesProvider
+
